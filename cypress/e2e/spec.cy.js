ac4c06ecbabb35b7a49853486bea2277c5945d6b
edwardhdez <ejhernandezcruz@gmail.com>
2022-08-26 10:45:11 -0400

    First test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('empty spec', () => {
+  it('Valid login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.shopping_cart_link').should('be.visible')
+  })
+})
