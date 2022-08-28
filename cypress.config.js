6e31e9f8c8f70664002a3fa0a0ca637e23ada880
edwardhdez <ejhernandezcruz@gmail.com>
2022-08-28 18:53:59 -0400

    prepare Browserstack conf

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,4 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  projectId: 'f8nz46',
-  chromeWebSecurity: false,
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-   
-  },
-});
+module.exports = {
+  'projectId': 'f8nz46',
+  e2e: {}
+}
