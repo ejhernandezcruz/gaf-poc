7b983db43cc3d8bbb58f05db6f26dd316b7297b9
ejhernandezcruz <ejhernandezcruz@gmail.com>
2022-11-22 13:12:47 -0400

    conditional workflow

--- scripts/test.js
+++ scripts/test.js
@@ -1,24 +1,16 @@
 import { exec } from 'child_process'
 
-const command = process.env.SPEC
+const command = process.argv[2];
 
 
-if (command === undefined) {
-    console.log("This is undefined");
-}
-
-else {
-
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
-
-}
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
